5e83cc4366b72bca9577f8a72c57e9c47c0e7c26
jesga06 <jessesouzaamorim@gmail.com>
2025-04-16 09:57:14 -0300

    h

--- 1045.py
+++ 1045.py
@@ -5,13 +5,38 @@ b = float(lados[1])
 c = float(lados[2])
 if a >= b+c:
     print("NAO FORMA TRIANGULO")
-if a ** 2 == (b ** 2) + (c ** 2):
-    print("TRIANGULO RETANGULO")
-if a ** 2 < (b ** 2) + (c ** 2):
-    print("TRIANGULO ACUTANGULO")
-if a ** 2 > (b ** 2) + (c ** 2):
-    print("TRIANGULO OBTUSANGULO")
-if a == b and a == c and b == c:
-    print("TRIANGULO EQUILATERO")
-if a == b or a == c or b == c:
-    print("TRIANGULO ISOSCELES")
+else:
+    if a ** 2 == (b ** 2) + (c ** 2):
+        print("TRIANGULO RETANGULO")
+    elif a ** 2 < (b ** 2) + (c ** 2):
+        print("TRIANGULO ACUTANGULO")
+    elif a ** 2 > (b ** 2) + (c ** 2):
+        print("TRIANGULO OBTUSANGULO")
+    if a == b and a == c and b == c:
+        print("TRIANGULO EQUILATERO")
+    if a == b or a == c or b == c:
+        print("TRIANGULO ISOSCELES")
+
+"""código q funciona de acordo com o gpt
+# Leitura e ordenação
+lados = list(map(float, input().split()))
+lados.sort(reverse=True)
+a, b, c = lados
+
+# Caso não forme triângulo
+if a >= b + c:
+    print("NAO FORMA TRIANGULO")
+else:
+    # Tipos de ângulo
+    if a*a == b*b + c*c:
+        print("TRIANGULO RETANGULO")
+    elif a*a > b*b + c*c:
+        print("TRIANGULO OBTUSANGULO")
+    else:  # a*a < b*b + c*c
+        print("TRIANGULO ACUTANGULO")
+    
+    # Tipos de lados
+    if a == b == c:
+        print("TRIANGULO EQUILATERO")
+    elif a == b or b == c or a == c:
+        print("TRIANGULO ISOSCELES")"""
